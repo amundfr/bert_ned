eaf994a087170156d6cfd75094825658c322318d
araaheim <amund.faller.raaheim@cern.ch>
2020-11-23 17:51:12 +0100

    first try at a tensorboard of BERT word embeddings

---
+++ main.py
@@ -0,0 +1,27 @@
+import transformers
+import numpy as np
+from torch.utils.tensorboard import SummaryWriter
+
+writer = SummaryWriter('runs/testing_tensorboard_pt')
+model = transformers.BertModel.from_pretrained('bert-base-uncased')
+
+tokenizer = transformers.BertTokenizer.from_pretrained('bert-base-uncased')
+words = tokenizer.vocab.keys()
+
+word_embedding = model.embeddings.word_embeddings.weight
+writer.add_embedding(word_embedding,
+                         metadata  = words,
+                        tag = f'word embedding')
+
+position_embedding = model.embeddings.position_embeddings.weight
+writer.add_embedding(position_embedding,
+                         metadata  = np.arange(position_embedding.shape[0]),
+                        tag = f'position embedding')
+
+token_type_embedding = model.embeddings.token_type_embeddings.weight
+writer.add_embedding(token_type_embedding,
+                         metadata  = np.arange(token_type_embedding.shape[0]),
+                        tag = f'tokentype embeddings')
+
+writer.close()
+
